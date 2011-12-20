30536ea81b1fc58094d584767b067ae73387b75e
Iain Ballard <ieb@me.com>
2011-12-20 15:57:49 +0000

    Control fading

--- SkeletonPlayer/MainPage.xaml.cs
+++ SkeletonPlayer/MainPage.xaml.cs
@@ -1,24 +1,16 @@
 ﻿using System;
 using System.Windows.Controls;
+using System.Windows.Threading;
 using jwSkinLoader;
 
 namespace JwslPlayer {
 	public partial class MainPage : UserControl {
-		private readonly HtmlInteraction bridge;
-		private readonly JwSkinPackage jwSkinPackage;
-		private string srcPlaylist = "";
-
-		public string SourcePlaylist {
-			set {
-				if (Player != null) {
-					Player.LoadPlaylist(value);
-				} else {
-					srcPlaylist = value;
-				}
-			}
-		}
-
-		public string SkinPackageUrl { set { jwSkinPackage.Load(value); } }
+		readonly HtmlInteraction bridge;
+		readonly JwSkinPackage jwSkinPackage;
+		string srcPlaylist = "";
+		const double ControlsFadeDelay = 3.0;
+		volatile bool ControlsAreFaded;
+		readonly OpacityFader controlBarFader;
 
 		public MainPage () {
 			InitializeComponent();
@@ -37,6 +29,9 @@ namespace JwslPlayer {
 			DisplayView.AddBinding(Player);
 			DockView.AddBinding(Player);
 			PlaylistView.AddBinding(Player);
+
+			controlBarFader = new OpacityFader(ControlBarView);
+			SetFadeTimer();
 		}
 
 		void JwSkinPackageSkinPackageReady(object sender, EventArgs e) {
@@ -44,6 +39,69 @@ namespace JwslPlayer {
 			DisplayView.SetSkin(jwSkinPackage);
 			DockView.SetSkin(jwSkinPackage);
 			PlaylistView.SetSkin(jwSkinPackage);
+		}
+
+
+		public string SourcePlaylist {
+			set {
+				if (Player != null) {
+					Player.LoadPlaylist(value);
+				} else {
+					srcPlaylist = value;
+				}
+			}
+		}
+
+		public string SkinPackageUrl { set { jwSkinPackage.Load(value); } }
+
+		private DispatcherTimer fadeTimer;
+		private void SetFadeTimer() {
+			if (fadeTimer != null) return;
+			fadeTimer = new DispatcherTimer();
+			fadeTimer.Tick += FadeDispatcherTimerTick;
+			fadeTimer.Interval = TimeSpan.FromSeconds(ControlsFadeDelay);
+			fadeTimer.Start();
+		}
+
+		void FadeDispatcherTimerTick(object sender, EventArgs e) {
+			if (Dispatcher.CheckAccess()) {
+				FadeControlBar();
+			} else {
+				Dispatcher.BeginInvoke(FadeControlBar);
+			}
+		}
+
+		void FadeControlBar() {
+			if (ControlsAreFaded) return;
+
+			if (!Player.IsPlayerActive())
+				return;
+
+			ControlsAreFaded = true;
+			controlBarFader.Out();
+		}
+
+		void UnfadeControlBar () {
+			if (!ControlsAreFaded) return;
+
+			ControlsAreFaded = false;
+			controlBarFader.In();
+		}
+
+		private void LayoutRoot_MouseEnter (object sender, System.Windows.Input.MouseEventArgs e) {
+			UnfadeControlBar();
+			fadeTimer.Start();
+		}
+
+		private void LayoutRoot_MouseLeave (object sender, System.Windows.Input.MouseEventArgs e) {
+			FadeControlBar();
+			fadeTimer.Stop();
+		}
+
+		private void LayoutRoot_MouseMove (object sender, System.Windows.Input.MouseEventArgs e) {
+			UnfadeControlBar();
+			fadeTimer.Stop();
+			fadeTimer.Start();
 		}
 	}
 }
---
+++ SkeletonPlayer/OpacityFader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace JwslPlayer {
+	public class OpacityFader {
+		protected Storyboard OverAnimation, OutAnimation;
+
+		public OpacityFader (DependencyObject target) {
+			Setup(target, 0.0, 1.0);
+		}
+
+		public OpacityFader (DependencyObject target, double lower, double upper) {
+			Setup(target, lower, upper);
+		}
+
+		private void Setup (DependencyObject target, double lower, double upper) {
+			OverAnimation = new Storyboard();
+			var ca = new DoubleAnimation{
+				To = upper,
+				Duration = TimeSpan.FromSeconds(0.2)
+			};
+			Storyboard.SetTarget(ca, target);
+			Storyboard.SetTargetProperty(ca, new PropertyPath("(UIElement.Opacity)"));
+			OverAnimation.Children.Add(ca);
+
+			OutAnimation = new Storyboard();
+			var ca2 = new DoubleAnimation{
+				To = lower,
+				Duration = TimeSpan.FromSeconds(0.2)
+			};
+			Storyboard.SetTarget(ca2, target);
+			Storyboard.SetTargetProperty(ca2, new PropertyPath("(UIElement.Opacity)"));
+			OutAnimation.Children.Add(ca2);
+		}
+
+		public void In () {
+			OverAnimation.Begin();
+		}
+
+		public void Out () {
+			OutAnimation.Begin();
+		}
+	}
+}
